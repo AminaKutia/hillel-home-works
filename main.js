5f648dbdf42dfd863500e5841c4d9cf9d523f1e5
AminaKutia <amina.kutia@gmail.com>
2026-01-28 19:14:41 +0100

    add index-html and main.js

--- main.js
+++ main.js
@@ -1,181 +1,276 @@
-/* 1. Порахувати кількість додатних чисел у масиві.
-Дано масив:
+console.log('#3. JavaScript homework example file')
 
-[3, -1, 0, 5, 7, -4]
+/*
+ * #1
+ *
+ * Створіть об'єкт userObj, що описує людину.
+ *
+ * Наступні поля обов'язкові:
+ * firstName - будь-яке ім'я, рядок
+ * lastName - будь-яке прізвище, рядок
+ * age - будь-який вік, число
+ */
 
-Порахуйте, скільки в ньому додатних чисел. */
+const userObj = {
+  firstName: 'John',
+  lastName: 'Smith',
+  age: 20
+};
 
-const arr1 = [3, -1, 0, 5, 7, -4];
-let count = 0;
+console.log(userObj)
 
-for (let i = 0; i < arr1.length; i++) {
-  if (arr1[i] > 0) {
-    count++;
+/*
+ * #2
+ *
+ * Для об'єкта з п.1 створіть метод fullName(), що повертає коректне повне ім'я, яке є конкатенацією firstName та lastName через пробіл.
+
+ * Наприклад:
+ * userObj.firstName ← 'John'
+ * userObj.lastName  ← 'Smith'
+ * userObj.fullName() → 'John Smith'.
+ */
+
+userObj.fullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+console.log(userObj.fullName()) // John Smith
+
+/*
+ * #3
+ *
+ * Функція defUpperStr('My text') повертає текст, перетворений у верхній регістр, тобто: defUpperStr('My text') → 'MY TEXT'.
+ *
+ * Якщо функція викликається без параметра defUpperStr(), вона не повинна повертати undefined, у цьому випадку потрібно повернути рядок тексту за замовчуванням у верхньому регістрі, тобто defUpperStr() → 'DEFAULT TEXT'.
+ *
+ * При виконанні завдання не використовуйте оператор if, потрібен розв'язок із логічним оператором ||.
+ */
+
+function defUpperStr(str) {
+  return (str || 'DEFAULT TEXT').toUpperCase();
+}
+
+console.log(defUpperStr('My text')) // MY TEXT
+console.log(defUpperStr())             // DEFAULT TEXT
+
+/*
+ * #4
+ *
+ * Створіть функцію evenFn(n), яка приймає параметром число - кількість ітерацій циклу, тобто for 0..n.
+ * Функція повинна повернути масив, що складається тільки з парних значень, які генеруються в циклі.
+ *
+ * Причому:
+ * 0 не повинен потрапляти до результуючого масиву,
+ * цикл має працювати до n включно,
+ * дозволено тільки оператор for.
+ *
+ * Наприклад:
+ * evenFn(10) → [2, 4, 6, 8, 10]
+ * evenFn(15) → [2, 4, 6, 8, 10, 12, 14]
+ * evenFn(20) → [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+ */
+
+function evenFn(n) {
+  const result = [];
+  for(let i = 1; i <= n; i++){
+    if (i % 2 === 0) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+console.log(evenFn(10)) // [2, 4, 6, 8, 10]
+console.log(evenFn(15)) // [2, 4, 6, 8, 10, 12, 14]
+console.log(evenFn(20)) // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+/*
+ * #5
+ *
+ * Створіть функцію weekFn(n), яка приймає номер дня тижня, а повертає його назву.
+ * Якщо вводиться рядок, будь-яке дробове число або число поза діапазоном 1...7 - функція повинна повернути null.
+ *
+ * Наприклад:
+ * 1   → 'Понеділок'
+ * 2   → 'Вівторок'
+ * ...
+ * 7   → 'Неділя'
+ * 9   → null
+ * 1.5 → null
+ * '2' → null
+ * У реалізації функції обов'язково мають бути використані оператори switch / case / default.
+ */
+
+function weekFn (n){
+  if (typeof n !== 'number' || !Number.isInteger(n)) {
+    return null;
+  }
+  switch (n){
+    case 1: return 'Понеділок';
+    case 2: return 'Вівторок';
+    case 3: return 'Середа';
+    case 4: return 'Четвер';
+    case 5: return 'Пʼятниця';
+    case 6: return 'Субота';
+    case 7: return 'Неділя';
+    default: return null;
   }
 }
-console.log(count);
+
+console.log(weekFn(1))   // 'Понеділок'
+console.log(weekFn(3))   // 'Середа'
+console.log(weekFn(7))   // 'Неділя'
+console.log(weekFn(9))   // null
+console.log(weekFn(1.5)) // null
+console.log(weekFn('2')) // null
+
+/*
+ * #6
+ *
+ * створіть функцію ageClassification(n), яка буде як параметр приймати будь-які числа і повертатиме рядок згідно з такими умовами, n:
+ * менше 0   - null (зверніть увагу, що це саме null, а не рядок)
+ * 0..24     - 'Дитинство'
+ * 24+...44  - 'Молодість'
+ * 44+..65   - 'Зрілість'
+ * 65+..75   - 'Старість'
+ * 75+..90   - 'Довголіття'
+ * 90+..122  - 'Рекорд'
+ * понад 122 - null (зверніть увагу, що це саме null, а не рядок)
+ *
+ * При виконанні завдання допускається використовувати тільки тернарний оператор ?.
+ * Використання операторів if, switch - заборонено.
+ */
+
+function ageClassification(n) {
+  return n < 0 || n > 122 ? null
+    : n === 0 ? null
+    : n <= 24 ? 'Дитинство'
+    : n <= 44 ? 'Молодість'
+    : n <= 65 ? 'Зрілість'
+    : n <= 75 ? 'Старість'
+    : n <= 90 ? 'Довголіття'
+    : 'Рекорд';
+}
+
+console.log('    -1 :', ageClassification(-1)) // -1 : null
+console.log('     0 :', ageClassification(0)) // 0 : null
+console.log('     1 :', ageClassification(1)) // 1 : Дитинство
+console.log('    24 :', ageClassification(24)) // 24 : Дитинство
+console.log(' 24.01 :', ageClassification(24.01)) // 24.01 : Молодість
+console.log('    44 :', ageClassification(44)) // 44 : Молодість
+console.log(' 44.01 :', ageClassification(44.01)) // 44.01 : Зрілість
+console.log('    65 :', ageClassification(65)) // 65 : Зрілість
+console.log('  65.1 :', ageClassification(65.1)) // 65.1 : Старість
+console.log('    75 :', ageClassification(75)) // 75 : Старість
+console.log(' 75.01 :', ageClassification(75.01)) // 75.01 : Довголіття
+console.log('    90 :', ageClassification(90)) // 90 : Довголіття
+console.log(' 90.01 :', ageClassification(90.01)) // 90.01 : Рекорд
+console.log('   122 :', ageClassification(122)) // 122 : Рекорд
+console.log('122.01 :', ageClassification(122.01)) // 122.01 : null
+console.log('   150 :', ageClassification(150)) // 150 : null
 
 
-/* 2. Створити новий масив тільки з парних чисел.
-Дано масив:
+// Блок тестирования, везде должны быть true:
+ console.log('    -1 :', ageClassification(-1) === null); // -1 : null
+ console.log('     0 :', ageClassification(0) === null) // 0 : null
+ console.log('     1 :', ageClassification(1) === 'Дитинство'); // 1 : Дитинство
+ console.log('    24 :', ageClassification(24) === 'Дитинство'); // 24 : Дитинство
+ console.log(' 24.01 :', ageClassification(24.01) === 'Молодість'); // 24.01 : Молодість
+ console.log('    44 :', ageClassification(44) === 'Молодість'); // 44 : Молодість
+ console.log(' 44.01 :', ageClassification(44.01) === 'Зрілість'); // 44.01 : Зрілість
+ console.log('    65 :', ageClassification(65) === 'Зрілість'); // 65 : Зрілість
+ console.log('  65.1 :', ageClassification(65.1) === 'Старість'); // 65.1 : Старість
+ console.log('    75 :', ageClassification(75) === 'Старість'); // 75 : Старість
+ console.log(' 75.01 :', ageClassification(75.01) === 'Довголіття'); // 75.01 : Довголіття
+ console.log('    90 :', ageClassification(90) === 'Довголіття'); // 90 : Довголіття
+ console.log(' 90.01 :', ageClassification(90.01) === 'Рекорд'); // 90.01 : Рекорд
+ console.log('   122 :', ageClassification(122) === 'Рекорд'); // 122 : Рекорд
+ console.log('122.01 :', ageClassification(122.01) === null); // 122.01 : null
+ console.log('   150 :', ageClassification(150) === null); // 150 : null
 
-[1, 4, 7, 10, 13, 16]
+/*
+ * #7
+ *
+ * Створіть функцію oddFn(n), яка приймає параметром число - кількість ітерацій циклу.
+ * Функція повинна повернути масив, що складається тільки з непарних значень, які генеруються в циклі.
+ *
+ * Причому:
+ * 0 не повинен потрапляти в результуючий масив,
+ * цикл має працювати до n включно,
+ * дозволено тільки оператор while.
+ *
+ * Наприклад:
+ * oddFn(10) → [1, 3, 5, 7, 9]
+ * oddFn(15) → [1, 3, 5, 7, 9, 11, 13, 15]
+ * oddFn(20) → [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+ */
 
-Створіть новий масив, у який потраплять тільки парні елементи. */
+function oddFn(n) {
+  const result = [];
+  let i = 1;
 
-const arr2 = [1, 4, 7, 10, 13, 16];
-const neuArr = [];
-
-for(let i = 0; i < arr2.length; i++){
-  if(arr2[i]%2 === 0){
-    neuArr.push(arr2[i]);
+  while (i <= n) {
+    if (i % 2 !== 0) {
+      result.push(i);
+    }
+    i++;
   }
+  return result;
 }
-console.log(neuArr);
 
-/* 3. Замінити всі від’ємні числа на 0.
-Дано масив:
+console.log(oddFn(10)) // [1, 3, 5, 7, 9]
+console.log(oddFn(15)) // [1, 3, 5, 7, 9, 11, 13, 15]
+console.log(oddFn(20)) // [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 
-[5, -3, 2, -8, 0]
+/*
+ * #8
+ *
+ * Створіть основну функцію mainFunc(a, b, callback), яка приймає три параметри:
+ * a - число,
+ * b - число,
+ * callback - функція зворотнього виклику, що обробляє параметри a і b.
+ *
+ * Реалізуйте перевірку: якщо третім параметром передається не функція, потрібно повернути false.
+ */
 
-Замініть усі від’ємні значення на 0. */
-
-const arr3 = [5, -3, 2, -8, 0];
-
-for(let i = 0; i < arr3.length; i++){
-  if(arr3[i] < 0){
-    arr3[i] = 0;
+function mainFunc(a, b, callback) {
+  if (typeof callback !== 'function') {
+    return false;
   }
-}
-console.log(arr3);
 
-/* 4. Перевірити, чи містить масив певний елемент.
-Дано масив:
+  return callback(a, b);
+ }
 
-["red", "green", "blue"]
+/*
+ * Реалізуйте callback функції (cbRandom, cbPow, cbAdd) до основної функції (mainFunc), що повертатимуть відповідні результати обчислень.
+ * Використовуйте Math для піднесення до ступеня (Math.pow) та генерації випадкових чисел (Math.floor, Math.random).
+ */
 
-Та значення: "green"
-
-Перевірте, чи міститься це значення в масиві. Виведіть "Знайдено" або "Не знайдено". */
-
-const arr4 = ["red", "green", "blue"];
-const value = "green";
-let result = false;
-
-for (let i = 0; i < arr4.length; i++) {
-  if (arr4[i] === value) {
-    result = true;
-    break;
-  }
+// cbRandom(a, b) - обчислює і повертає довільне ціле число в діапазоні між a і b включно.
+function cbRandom(min, max) { 
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(result ? "Знайдено" : "Не знайдено");
-
-/* 5. Видалити перший нуль у масиві.
-Дано масив:
-
-[4, 0, 5, 0, 2]
-
-Видаліть перший елемент зі значенням 0. */
-
-let arr5 = [4, 0, 5, 0, 2];
-let deleted = false;
-
-for(let i = 0; i < arr5.length; i++){
-  if(arr5[i] === 0){
-    arr5.splice(i, 1);
-    break;
-  }
-}
-console.log(arr5);
-
-/* 6. Перетворити масив слів у рядок через кому.
-Дано масив:
-
-["apple", "banana", "pear"]
-
-Створіть рядок "apple, banana, pear" використовуючи цикл. */
-
-let arr6 = ["apple", "banana", "pear"];
-let result1 = "";
-
-for(let i = 0; i < arr6.length; i++){
-  result1 += arr6[i];
-  if(i < arr6.length - 1) {
-    result1 += ", ";
-  }
-}
-console.log(result1);
-
-/* 7. Знайти мінімальне число в масиві.
-Дано масив:
-
-[9, 2, 14, -5, 7]
-
-Знайдіть найменший елемент без використання Math.min. */
-
-const arr7 = [9, 2, 14, -5, 7];
-let min = arr7[0];
-
-for(let i = 0; i < arr7.length; i++){
-  if(arr7[i] < min){
-    min = arr7[i];
-  }
-}
-console.log(min);
-
-/* 8. Порахувати кількість входжень заданого елемента.
-Дано масив:
-
-["a", "b", "a", "c", "a"]
-
-Та елемент: "a"
-
-Порахуйте, скільки разів "a" зустрічається у масиві. */
-
-const arr8 = ["a", "b", "a", "c", "a"];
-let search = "a";
-let count1 = 0;
-
-for(let i = 0; i < arr8.length; i++){
-  if(arr8[i] === search){
-    count1++;
-  }
-}
-console.log(count1);
-
-/* 9. Додати число в початок або кінець масиву.
-Дано масив:
-
-[5, 7, 9]
-
-Та число: 12
-
-Якщо число більше за 10 — додайте в кінець масиву, інакше — у початок. */
-
-const arr9 = [5, 7, 9];
-const num = 12;
-
-if(num > 10){
-  arr9.push(num);
-}
-else{
-  arr9.unshift(num);
-}
-console.log(arr9);
-
-/* 10. Створити масив довжини n, заповнений одиницями.
-Дано число:
-
-n = 5
-
-Створіть масив довжини 5, заповнений значенням 1 . */
-
-const arr10 = [];
-const n = 5;
-
-for(let i = 0; i < n; i++){
-  arr10.push(1);
+// cbPow(a, b) - обчислює і повертає результат піднесення числа a у ступінь b.
+function cbPow(num, pow) { 
+  return Math.pow(num, pow);
 }
 
-console.log(arr10);
+// cbAdd(a, b) - обчислює і повертає суму двох чисел a і b.
+function cbAdd(a, b) { 
+  return a + b;
+}
+
+/*
+ * mainFunc() повинна повертати результат роботи переданої їй поворотної функції, наприклад:
+ * mainFunc(2, 5, cbRandom) → випадково від 2 до 5 включно
+ * mainFunc(10, 30, cbRandom) → випадково 10..30 включно
+ * mainFunc(2, 5, cbPow) → 32
+ * mainFunc(2, 5, cbAdd) → 7
+ * mainFunc(2, 5, 'not a func') → false
+ */
+
+console.log(mainFunc(2, 5, cbRandom)) // цілі числа в діапазоні 2..5
+console.log(mainFunc(2, 5, cbPow)) // 32
+console.log(mainFunc(2, 5, cbAdd)) // 7
+console.log(mainFunc(2, 5, 'not a func')) // false
